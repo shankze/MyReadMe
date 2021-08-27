a0a2b4838e7f0295c8dd21ac54f5419b936351da
shashank_bekal <shashank.bekal@slalom.com>
2021-08-26 22:15:30 -0500

    updated by user 2

--- index.js
+++ index.js
@@ -2,5 +2,6 @@ let text = "Updated from user dev branch";
 let text_2 = "Updated from feature branch";
 let text_3 = "Updated from feature branch";
 let text_4 = "Updated from feature branch";
+let text_5 = "Updated from feature branch 2";
 let version = 1;
 console.log(text);
